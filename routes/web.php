850bb32e1748422e247d7e3bd3fd3d9dee6806e5
asfim <asfim5233@gmail.com>
2026-06-13 17:52:25 +0600

    admin login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +12,13 @@ Route::post('/contact', [PageController::class, 'contactSubmit'])->name('contact
 Route::post('/get-started', [PageController::class, 'getStartedSubmit'])->name('get-started.submit');
 Route::get('/blog', [PageController::class, 'blog'])->name('blog');
 
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+});
 
 Route::prefix('services')->name('services.')->group(function () {
     Route::get('/web-application', [PageController::class, 'webApplication'])->name('web-app');
